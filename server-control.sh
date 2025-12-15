c52b8ace38c5d96fbd20074b08ea04c13ef8dcf9
Conner Smith <connersoftdev@gmail.com>
2025-12-15 03:21:45 -0600

    Update server-control.sh
    Old version didn't make directories. I forgot to add that in lol

--- server-control.sh
+++ server-control.sh
@@ -5,6 +5,28 @@ VM_DOMAIN="ubuntu24.04-NFS" # Put the name of your VM here. This is for the VM r
 NBD_DEV="/dev/nbd0"
 NFS_MOUNT="/var/www/html/os/kubuntu" # Change this to your NFS mount point if needed
 RAM_DIR="/var/www/html/os/kubuntu-ram" # Change this to your RAM boot mount point if needed 
+IMAGE_PATH="/var/lib/libvirt/images/example.qcow2" # Change this to your VM disk image!!
+
+function check_and_create_dirs() {
+    echo "--- CHECKING DIRECTORIES ---"
+    
+    # Check NFS_MOUNT
+    if [ ! -d "$NFS_MOUNT" ]; then
+        echo "Directory $NFS_MOUNT does not exist. Creating it..."
+        sudo mkdir -p "$NFS_MOUNT"
+    else
+        echo "Directory $NFS_MOUNT exists."
+    fi
+
+    # Check RAM_DIR
+    if [ ! -d "$RAM_DIR" ]; then
+        echo "Directory $RAM_DIR does not exist. Creating it..."
+        sudo mkdir -p "$RAM_DIR"
+    else
+        echo "Directory $RAM_DIR exists."
+    fi
+    echo "------------------------------"
+}
 
 function set_production() {
     echo "--- SWITCHING TO PRODUCTION MODE ---"
@@ -17,14 +39,26 @@ function set_production() {
     # 2. Connect NBD and Mount NFS
     echo "Connecting VM disk for NFS..."
     sudo modprobe nbd max_part=16
-    # Added --persistent to prevent timeouts and -f qcow2 to be explicit
-    sudo qemu-nbd --connect=$NBD_DEV --persistent -f qcow2 /var/lib/libvirt/images/ubuntu24.04-NFS.qcow2 # Change to your VMs hard drive image path
+    
+    sudo qemu-nbd --connect=$NBD_DEV --persistent -f qcow2 ${IMAGE_PATH} 
     sleep 2
     sudo mount ${NBD_DEV}p1 $NFS_MOUNT
+    sleep 2
     
+    # Identify the latest kernel and initrd (sort by version, take the last one)
+    LATEST_VMLINUZ=$(ls -1 ${NFS_MOUNT}/boot/vmlinuz* | sort -V | tail -n 1)
+    LATEST_INITRD=$(ls -1 ${NFS_MOUNT}/boot/initrd.img* | sort -V | tail -n 1)
+
+    # Copy and rename them
+    echo "Copying $LATEST_VMLINUZ to ${NFS_MOUNT}/vmlinuz..."
+    sudo cp "$LATEST_VMLINUZ" "${NFS_MOUNT}/vmlinuz"
+
+    echo "Copying $LATEST_INITRD to ${NFS_MOUNT}/initrd.img..."
+    sudo cp "$LATEST_INITRD" "${NFS_MOUNT}/initrd.img"
+
     # 3. Refresh Exports
     sudo exportfs -r
-    
+     
     echo "Done. NFS and RAM services are BOTH ACTIVE."
 }
 
